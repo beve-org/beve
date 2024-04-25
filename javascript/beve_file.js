b1ebf08c41dc0dcd2cb67d6c5719ec99fe732087
Stephen Berry <stephenberry.developer@gmail.com>
2024-04-25 13:34:05 -0500

    renaming load_beve beve_file.js

--- javascript/beve_file.js
+++ javascript/beve_file.js
@@ -1,6 +1,9 @@
+// Reference: https://github.com/stephenberry/beve
+
 const fs = require('fs');
 
-function load_beve(filename) {
+// Reading BEVE
+function read_beve(filename) {
     // Open dialog box if filename isn't provided.
     if (!filename) {
         throw new Error('No filename provided.');
@@ -341,12 +344,211 @@ function read_compressed(fid) {
     }
 }
 
+// Writing BEVE
+function write_beve(data, filename) {
+    const file = new BinaryFile(filename);
+    if (!file) {
+        throw new Error('Failed to open file for writing');
+    }
+    try {
+        write_value(file, data);
+    } finally {
+        file.close();
+    }
+}
+
+function write_value(file, value) {
+    if (Array.isArray(value) && value.length > 1) {
+        const header = 4;
+        if (typeof value[0] === 'number' && !Number.isInteger(value[0])) {
+            write_float(file, header, value, 1);
+        } else {
+            write_integer(file, header, value, 1);
+        }
+    } else if (typeof value === 'boolean') {
+        let header = 0;
+        if (value) {
+            header |= 0b00011000;
+        } else {
+            header |= 0b00001000;
+        }
+        file.writeUint8(header);
+    } else if (typeof value === 'number') {
+        let header = 1;
+        if (!Number.isInteger(value)) {
+            write_float(file, header, value, 0);
+        } else {
+            write_integer(file, header, value, 0);
+        }
+    } else if (typeof value === 'string') {
+        throw new Error('Unsupported data type');
+    } else if (typeof value === 'object' && Object.keys(value).length > 0) {
+        let header = 3;
+        let keyType = 0; // Assuming keys are always strings
+        let isSigned = false;
+        header |= keyType << 3;
+        header |= isSigned << 5;
+        file.writeUint8(header);
+        writeCompressed(file, Object.keys(value).length);
+        for (const key in value) {
+            writeCompressed(file, key.length);
+            file.writeString(key);
+            write_value(file, value[key]);
+        }
+    } else if (Array.isArray(value)) {
+        let header = 5;
+        file.writeUint8(header);
+        writeCompressed(file, value.length);
+        for (let i = 0; i < value.length; i++) {
+            write_value(file, value[i]);
+        }
+    } else {
+        throw new Error('Unsupported data type');
+    }
+}
+
+function write_float(file, header, value, isArray) {
+    if (Math.fround(value) === value) { // single precision float
+        header |= 0b01000000;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.write_float32LE(value);
+    } else { // double precision float
+        header |= 0b01100000;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.write_float64LE(value);
+    }
+}
+
+function write_integer(file, header, value, isArray) {
+    if (value >= 0 && value <= 255) { // uint8
+        header |= 0b00010001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeUint8(value);
+    } else if (value >= 0 && value <= 65535) { // uint16
+        header |= 0b00110001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeUint16LE(value);
+    } else if (value >= 0 && value <= 4294967295) { // uint32
+        header |= 0b01010001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeUint32LE(value);
+    } else if (value >= 0 && value <= Number.MAX_SAFE_INTEGER) { // uint64
+        header |= 0b01110001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeBigInt64LE(BigInt(value));
+    } else if (value >= -128 && value <= 127) { // int8
+        header |= 0b00001001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeInt8(value);
+    } else if (value >= -32768 && value <= 32767) { // int16
+        header |= 0b00101001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+    } else if (value >= -2147483648 && value <= 2147483647) { // int32
+        header |= 0b01001001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeInt32LE(value);
+    } else if (value >= -9223372036854775808 && value <= 9223372036854775807) { // int64
+        header |= 0b01101001;
+        file.writeUint8(header);
+        if (isArray) {
+            writeCompressed(file, value.length);
+        }
+        file.writeBigInt64LE(BigInt(value));
+    } else {
+        throw new Error('Unsupported type');
+    }
+}
+
+function writeCompressed(file, N) {
+    if (N < 64) {
+        const compressed = (N << 2) | 0;
+        file.writeUint8(compressed);
+    } else if (N < 16384) {
+        const compressed = (N << 2) | 1;
+        file.writeUint16LE(compressed);
+    } else if (N < 1073741824) {
+        const compressed = (N << 2) | 2;
+        file.writeUint32LE(compressed);
+    } else if (N < 4611686018427387904) {
+        const compressed = (N << 2) | 3;
+        file.writeUint64LE(BigInt(compressed));
+    }
+}
+
+class BinaryFile {
+    constructor(filename) {
+        this.filename = filename;
+        this.fd = require('fs').openSync(filename, 'wb');
+    }
+    writeUint8(value) {
+        require('fs').writeSync(this.fd, Buffer.from([value]));
+    }
+    writeUint16LE(value) {
+        require('fs').writeSync(this.fd, Buffer.from([value & 0xFF, (value >> 8) & 0xFF]));
+    }
+    writeUint32LE(value) {
+        require('fs').writeSync(this.fd, Buffer.from([(value >> 0) & 0xFF, (value >> 8) & 0xFF, (value >> 16) & 0xFF, (value >> 24) & 0xFF]));
+    }
+    writeUint64LE(value) {
+        require('fs').writeSync(this.fd, Buffer.from([(value >> 0n) & BigInt(0xFF), (value >> 8n) & BigInt(0xFF), (value >> 16n) & BigInt(0xFF), (value >> 24n) & BigInt(0xFF), (value >> 32n) & BigInt(0xFF), (value >> 40n) & BigInt(0xFF), (value >> 48n) & BigInt(0xFF), (value >> 56n) & BigInt(0xFF)]));
+    }
+    write_float32LE(value) {
+        const buffer = Buffer.alloc(4);
+        buffer.write_floatLE(value, 0);
+        require('fs').writeSync(this.fd, buffer);
+    }
+    write_float64LE(value) {
+        const buffer = Buffer.alloc(8);
+        buffer.writeDoubleLE(value, 0);
+        require('fs').writeSync(this.fd, buffer);
+    }
+    writeBigInt64LE(value) {
+        const buffer = Buffer.alloc(8);
+        buffer.writeBigInt64LE(value, 0);
+        require('fs').writeSync(this.fd, buffer);
+    }
+    writeString(str) {
+        require('fs').writeSync(this.fd, Buffer.from(str));
+    }
+    close() {
+        require('fs').closeSync(this.fd);
+    }
+}
+
 // Usage example:
 try {
     const filename = '../example/example.beve';
-    const data = load_beve(filename);
+    const data = read_beve(filename);
     console.log(data);
     //console.log(JSON.stringify(data));
+    //write_beve(data, '../example/examplejs.beve');
 } catch (error) {
     console.error(error);
 }
